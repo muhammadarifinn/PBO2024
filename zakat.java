ba08471d1e6b3172c27a79fc8b6cdc0738471c8f
muhammadarifinn <140833779+muhammadarifinn@users.noreply.github.com>
2024-10-21 19:41:06 +0700

    Update zakat.java

--- zakat.java
+++ zakat.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class zakat {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        // while (input > 0 )  {
-        //     System.out.print("Masukkan Jumlah Uang yang dimiliki (Rp.): ");
-        //     double harta = scanner.nextDouble();
-        //     double zmal = harta * 0.25;
-        //     System.out.println("Zakat yang harus dibayar adalah Rp. " +zmal);
-        //     System.out.print("=================================================\n");
-        // }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("PROGRAM MENGHITUNG ZAKAT MAL");
+        System.out.print("Masukkan NIM : ");
+        int input = scanner.nextInt();
+        while (input > 0 )  {
+            System.out.print("Masukkan Jumlah Uang yang dimiliki (Rp.): ");
+            double harta = scanner.nextDouble();
+            double zmal = harta * 0.25;
+            System.out.println("Zakat yang harus dibayar adalah Rp. " +zmal);
+            System.out.print("=================================================\n");
+        }
     }
 }
